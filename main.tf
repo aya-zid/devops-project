12cde444ed7d03a3a8b5e588563a27634bbec7dd
aya-zid <azid28278@gmail.com>
2025-05-02 22:23:31 +0100

    added terraform

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "myapp" {
+  name = "myapp"
+  build {
+    context = "${path.module}"
+  }
+}
+
+resource "docker_container" "myapp" {
+  name  = "myapp"
+  image = docker_image.myapp.name
+  ports {
+    internal = 5000
+    external = 5000
+  }
+}
